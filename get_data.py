e7d047cfe9b4e7a5744bf48d2b6e5b2d58f9c0d7
Tyler Freitas <tjfr8s@gmail.com>
2018-09-07 13:41:57 -0700

    Resample data over 10D using AdjClose to OHLC. Create candlestick plot and fill_between plot.

--- get_data.py
+++ get_data.py
@@ -1,10 +1,15 @@
 import quandl
-quandl.ApiConfig.api_key = "Z5F88HqyZ_2ApQUK9cMc"
+## doesn't work with current version of pandas_datareader,
+## must set envrionment variable QUANDL_API_KEY instead
+## quandl.ApiConfig.api_key = "Z5F88HqyZ_2ApQUK9cMc"
+
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
 
 ## Create dataframe contining stock information using quantl API
 ##style.use('ggplot')
@@ -16,14 +21,31 @@ import pandas_datareader.data as web
 df = pd.read_csv('tsla.csv', parse_dates = True, index_col = 0)
 df.drop(["AdjOpen", "ExDividend", "SplitRatio", "AdjHigh",
          "AdjVolume"], axis = 1, inplace = True)
-df['100ma'] = df['AdjClose'].rolling(window=100, min_periods=0).mean()
-print(df.head())
+
+#### Calculate 100 moving average and add to dataframe
+#### Create subplots plot various parts of data
+##df['100ma'] = df['AdjClose'].rolling(window=100, min_periods=0).mean()
+##print(df.head())
+##
+
+
+
+
+
+## OHLC data creation
+df_ohlc = df['AdjClose'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+
+df_ohlc.reset_index(inplace = True)
+
+# Convert dates to mdates for use with matplotlib
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
 
 ax1 = plt.subplot2grid((6, 1), (0,0), rowspan = 5, colspan = 1)
 ax2 = plt.subplot2grid((6, 1), (5,0), rowspan = 1, colspan = 1, sharex = ax1)
+ax1.xaxis_date()
 
-ax1.plot(df.index, df['AdjClose'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
-
+candlestick_ohlc(ax1, df_ohlc.values, width = 2, colorup = 'g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 plt.show()
